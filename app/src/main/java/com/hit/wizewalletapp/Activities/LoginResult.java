c550a439c0e5634f82699387142a15422dc33ea3
BenG <zeass973@gmail.com>
2022-04-12 19:02:25 +0300

    Ben: add Register user with Retrofit. send the request to the server in Node.js save in the mongoDB

---
+++ app/src/main/java/com/hit/wizewalletapp/Activities/LoginResult.java
@@ -0,0 +1,17 @@
+package com.hit.wizewalletapp.Activities;
+
+//for the Retrofit interface
+public class LoginResult {
+
+    private String email;
+
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
