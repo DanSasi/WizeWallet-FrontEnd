c81eef6546e8f4b19d5a0a67e10de8c05a636135
DanSasi <dan.sasi60@gmail.com>
2022-04-25 20:23:33 +0300

    merge to master update .

--- app/src/main/java/com/hit/wizewalletapp/Main/General_Folder/GeneralFragments/SignupTabFragment.java
+++ app/src/main/java/com/hit/wizewalletapp/Main/General_Folder/GeneralFragments/SignupTabFragment.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.hit.wizewalletapp.Main.General_Folder.GeneralActivites.LoginActivity;
 import com.hit.wizewalletapp.Main.General_Folder.GeneralActivites.RetrofitInterface;
 import com.hit.wizewalletapp.Main.Parent_Folder.Activities.ParentBalanceActivity;
 import com.hit.wizewalletapp.R;
@@ -71,10 +72,10 @@ public class SignupTabFragment extends Fragment {
                         //code 200 is OK , 400 not
                         if (response.code() == 200) {
                             Toast.makeText(v.getContext(), "register OK", Toast.LENGTH_LONG).show();
-                            Intent intent = new Intent(getActivity(), ParentBalanceActivity.class);
+                            Intent intent = new Intent(getActivity(), LoginActivity.class);
                             startActivity(intent);
                         } else if (response.code() == 400) {
-                            Toast.makeText(v.getContext(), "wrong email or password/already have user", Toast.LENGTH_LONG).show();
+                            Toast.makeText(v.getContext(), "wrong email or password/alre have user", Toast.LENGTH_LONG).show();
                         }
                     }
 
