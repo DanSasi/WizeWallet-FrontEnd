76ae4472ad165b36b84eaf96cbab3c46c08205f2
YardenMirko <83639847+YardenMirko@users.noreply.github.com>
2022-05-17 20:35:45 +0300

    adding spinner to send money fragment

--- app/src/main/java/com/hit/wizewalletapp/Main/Parent_Folder/Fragments/SendMoneyScreenFragment.java
+++ app/src/main/java/com/hit/wizewalletapp/Main/Parent_Folder/Fragments/SendMoneyScreenFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -17,7 +18,13 @@ import android.widget.TextView;
 
 import com.ebanx.swipebtn.OnStateChangeListener;
 import com.ebanx.swipebtn.SwipeButton;
+import com.hit.wizewalletapp.Main.Child_Folder.Models.Models.TaskChildModel;
 import com.hit.wizewalletapp.Main.Parent_Folder.Models.ListModels.ChildListModel;
+import com.hit.wizewalletapp.adapters.child.ChildAdapterSpinner;
+import com.hit.wizewalletapp.adapters.child.ChildTaskListAdapter;
+import com.hit.wizewalletapp.api.ApiCallsHelper;
+import com.hit.wizewalletapp.api.CustomCallBack;
+import com.hit.wizewalletapp.utilities.CacheUtilities;
 import com.hit.wizewalletapp.views.activity.PaymentSplashScreen;
 import com.hit.wizewalletapp.Main.Child_Folder.Models.Models.ChildModel;
 import com.hit.wizewalletapp.R;
@@ -27,14 +34,14 @@ import java.util.List;
 
 public class SendMoneyScreenFragment extends Fragment {
 
-
-    private Spinner spinner_members;
+    private final ChildTaskListAdapter listAdapter = new ChildTaskListAdapter();
     //private static ChildListScreenFragment.MyChildAdapter adapter;
     ImageView arr;
     static List<ChildModel> childList;
     TextView nameTxt;
     ImageView photo;
     TextView bankTxt;
+    Spinner spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,9 +53,8 @@ public class SendMoneyScreenFragment extends Fragment {
 
 
         SwipeButton swipeButton = view.findViewById(R.id.swipeId);
-        spinner_members = view.findViewById(R.id.spinner_mambers);
+        spinner = view.findViewById(R.id.send_money_spinner);
         ChildAdapter childAdapter = new ChildAdapter(getContext(),childList);
-        spinner_members.setAdapter(childAdapter);
 
 
 
@@ -58,6 +64,8 @@ public class SendMoneyScreenFragment extends Fragment {
                 startActivity(new Intent(getActivity(), PaymentSplashScreen.class ));
             }
         });
+        setSpinner(spinner);
+
 
 
         arr = view.findViewById(R.id.IV01);
@@ -113,6 +121,48 @@ public class SendMoneyScreenFragment extends Fragment {
 
             return rootView;
         }
+    }
+    private void setSpinner(Spinner spinner) {
+        ChildAdapterSpinner childAdapter = new ChildAdapterSpinner(getContext());
+        spinner.setAdapter(childAdapter);
+        ApiCallsHelper.performGetAllChilds( CacheUtilities.getAcssesToken(requireContext()), new CustomCallBack<List<ChildModel>>() {
+            @Override
+            public void onSuccesses(List<ChildModel> response) {
+                childAdapter.updateList(response);
+            }
+            @Override
+            public void onFailure(String msg) {
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectChildId = String.valueOf(childAdapter.getItem(position));
+                fetchTransactionsForChild(selectChildId);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void fetchTransactionsForChild(String selectChildId) {
+        String token= CacheUtilities.getAcssesToken(requireContext());
+        ApiCallsHelper.performGetAllTaskForChildByParent(token, selectChildId, new CustomCallBack<List<TaskChildModel>>() {
+            @Override
+            public void onSuccesses(List<TaskChildModel> response) {
+                listAdapter.updateTaskList(response);
+            }
+
+            @Override
+            public void onFailure(String msg) {
+
+            }
+        });
+
+
     }
 
 
